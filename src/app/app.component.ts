3aaa7a1e22db7c0ed6c365e21be799a773eacccb
hosseinamir <45227858+Hosseinamir1989@users.noreply.github.com>
2024-04-08 18:32:38 +0200

    feat: add home page RP-44 (#3)
    * feat: add header components and logo image RP-44
    
    * feat: add homepage components and page image RP-44
    
    * feat: add footer components RP-44
    
    * feat: add new components to app component RP-44

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./header/header.component";
+import {HomepageComponent} from "./homepage/homepage.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HomepageComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
