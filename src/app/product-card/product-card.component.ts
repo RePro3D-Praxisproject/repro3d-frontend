6b0e5846529ef45ef1c067f35194e48a2f62d8fa
mahlibalazs <mahli.balazs@outlook.hu>
2024-04-15 16:05:33 +0200

    fix: remove duplicate codes and add input param to component

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,19 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input, NO_ERRORS_SCHEMA } from '@angular/core';
 import {HeaderComponent} from "../header/header.component";
 import {FooterComponent} from "../footer/footer.component";
 import {FormsModule} from "@angular/forms";
-import {NgForOf, NgIf} from "@angular/common";
-import {Router, RouterLink, RouterLinkActive} from "@angular/router";
-export interface Product {
-  id: number;
-  title: string;
-  price: number;
-  buildTime: string;
-  material: string;
-  dimensions: string;
-  imgUrl: string;
-  description: string;
-}
+import { RouterLink } from "@angular/router";
+import { Item } from "../../interfaces/item";
+
 @Component({
   selector: 'app-product-card',
   standalone: true,
@@ -21,41 +12,16 @@ export interface Product {
     HeaderComponent,
     FooterComponent,
     FormsModule,
-    NgForOf,
     RouterLink,
-    RouterLinkActive,
-    NgIf
   ],
   templateUrl: './product-card.component.html',
-  styleUrl: './product-card.component.scss'
+  styleUrl: './product-card.component.scss',
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class ProductCardComponent {
 
-  searchTerm: string = '';
+  @Input({required: true}) product!: Item;
 
-
-  products: Product[] = [
-    { id: 1, title: 'ReProRing', price: 126, buildTime: 'est. 1 Hour', material: 'ABS', dimensions: '209 x 209 x 400', imgUrl: 'assets/picture-page.png', description:"This ring is the first product of the ReProd3d production and have so much value to us! if you print this item you will get 50 percent discount for that" },
-    { id: 2, title: 'skull', price: 126, buildTime: 'est. 1 Hour', material: 'ABS', dimensions: '209 x 209 x 400', imgUrl: 'assets/skull-picture.jpg', description:"This skull is the first product of the ReProd3d production and have so much value to us! if you print this item you will get 50 percent discount for that" },
-
-    { id: 3, title: 'John the thinker', price: 126, buildTime: 'est. 1 Hour', material: 'ABS', dimensions: '209 x 209 x 400', imgUrl: 'assets/john-picture.jpg', description:"This john is the first product of the ReProd3d production and have so much value to us! if you print this item you will get 50 percent discount for that" },
-    { id: 4, title: 'baby yoda', price: 150, buildTime: 'est. 2 Hour', material: 'ABS', dimensions: '209 x 209 x 400', imgUrl: 'assets/babyYoda-picture.jpg', description:"This baby yoda is the first product of the ReProd3d production and have so much value to us! if you print this item you will get 50 percent discount for that" },
-    { id: 5, title: 'dice', price: 140, buildTime: 'est. 3 Hour', material: 'ABS', dimensions: '209 x 209 x 400', imgUrl: 'assets/dice-picture.jpg', description:"This dice is the first product of the ReProd3d production and have so much value to us! if you print this item you will get 50 percent discount for that" },
-
-
-  ];
-
-
-  get filteredProducts(): Product[] {
-    return this.products.filter(product =>
-      product.title.toLowerCase().includes(this.searchTerm.toLowerCase())
-    );
-  }
-
-  constructor(
-    public router: Router
-  ) { }
-  ngOnInit(): void {
-  }
+  constructor() { }
 
 }
