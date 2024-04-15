7cbc847d1bd84fede50de142def14519d76f4822
mahlibalazs <mahli.balazs@outlook.hu>
2024-04-15 16:07:47 +0200

    feat: add interface for items

---
+++ src/interfaces/item.ts
@@ -0,0 +1,11 @@
+export interface Item {
+    itemId: number,
+    title: string,
+    price: number,
+    buildTime: number,
+    material: string,
+    dimensions: string,
+    description: string,
+    // TODO remove this
+    imgUrl: string
+}
