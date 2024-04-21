07a9d0452d34993c77567b75c17b6a77e1f28b96
mahlibalazs <mahli.balazs@outlook.hu>
2024-04-21 19:25:24 +0200

    feat: add login component RP-22

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,48 @@
+import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'; 
+import { AuthService } from '../shared/services/auth.service';
+import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule, NgIf],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  public readonly loginFormGroup: FormGroup;
+
+  public loginFailed: boolean;
+
+  constructor(private readonly formBuilder: FormBuilder, private authService: AuthService, private router: Router) {
+    this.loginFormGroup = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+    this.loginFailed = false;
+  }
+
+  public async onSubmit() {
+    if (this.loginFormGroup.invalid) {
+      alert('Invalid input');
+    } else {
+      this.authService.login(this.loginFormGroup.getRawValue().username, this.loginFormGroup.getRawValue().password).subscribe(
+        data => {
+          localStorage.setItem('email', this.loginFormGroup.getRawValue().username);
+          localStorage.setItem('password', this.loginFormGroup.getRawValue().password);
+          console.log('Login successful');
+        },
+        error => {
+          this.loginFailed = true;
+          this.authService.logout();
+          console.error('Login failed');
+        }
+      );
+    }
+      
+  }
+
+}
