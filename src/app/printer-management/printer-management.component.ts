5326022899a9779ce24a433b0cb1bd2af396ea1d
Hosseinamir1989 <hosseinamir198968@gmail.com>
2024-04-22 23:23:59 +0200

    feat: add printer functionality with simulated data and http methods RP-63

--- src/app/printer-management/printer-management.component.ts
+++ src/app/printer-management/printer-management.component.ts
@@ -1,21 +1,105 @@
-import { Component } from '@angular/core';
-import {NgForOf, NgIf} from "@angular/common";
+import {Component, OnInit} from '@angular/core';
+import {NgForOf, NgIf, NgStyle} from "@angular/common";
+import {PrinterService} from "../../services/printer-service.service";
+import {Printer} from "../../interfaces/printer";
+import {FormsModule} from "@angular/forms";
+
+
+
 
 @Component({
   selector: 'app-printer-management',
   standalone: true,
   imports: [
     NgForOf,
-    NgIf
+    NgIf,
+    FormsModule,
+    NgStyle
   ],
   templateUrl: './printer-management.component.html',
-  styleUrl: './printer-management.component.scss'
+  styleUrls: ['./printer-management.component.scss']
 })
-export class PrinterManagementComponent {
+export class PrinterManagementComponent implements OnInit{
+  printers: Printer[] = [];
   isModalOpen: boolean = false;
+  currentPrinter: Printer = new Printer();
+
+  constructor(private printerService: PrinterService) {
+
+  }
+
 
   toggleModal(open: boolean): void {
     this.isModalOpen = open;
   }
+/*
+  loadAllPrinters(): void {
+    this.printerService.getAllPrinters().subscribe({
+      next: (printers) => {
+        this.printers = printers;
+        console.log('Printers loaded:', printers);
+      },
+      error: (err) => console.error('Failed to load printers', err)
+    });
+  }
+  addPrinter(): void {
+    this.printerService.createPrinter(this.currentPrinter).subscribe({
+      next: () => {
+        this.loadAllPrinters();
+        this.toggleModal(false);
+        this.currentPrinter = new Printer();  // Reset after adding
+      },
+      error: (err) => console.error('Error adding printer', err)
+    });
+  }
 
+  updatePrinter(): void {
+    this.printerService.updatePrinter(this.currentPrinter.printer_id, this.currentPrinter).subscribe({
+      next: () => {
+        this.loadAllPrinters();
+        this.toggleModal(false);
+      },
+      error: (err) => console.error('Error updating printer', err)
+    });
+  }
+
+  deletePrinter(id: number | undefined): void {
+    this.printerService.deletePrinter(id).subscribe({
+      next: () => this.loadAllPrinters(),
+      error: (err) => console.error('Error deleting printer', err)
+    });
+  }
+
+  onEditPrinter(printer: Printer): void {
+    this.currentPrinter = { ...printer };
+    this.toggleModal(true);
+  }*/
+
+  ngOnInit(): void {
+    this.loadAllPrinters();
+
+  }
+
+  loadAllPrinters(): void {
+    this.printers = this.printerService.getAllPrinters();
+  }
+
+  addPrinter(printer: Printer): void {
+    this.printerService.createPrinter(printer);
+    this.loadAllPrinters();
+  }
+
+  updatePrinter(printer: Printer): void {
+    this.printerService.updatePrinter(printer);
+    this.loadAllPrinters();
+  }
+
+  deletePrinter(id: number | undefined): void {
+    this.printerService.deletePrinter(id);
+    this.loadAllPrinters();
+  }
+
+  onEditPrinter(printer: Printer) {
+
+  }
 }
---
+++ src/interfaces/printer.ts
@@ -0,0 +1,7 @@
+export class Printer {
+  printer_id?: number;
+  name?: string;
+  location?: string;
+  ip_addr?: string;
+  apikey?: string;
+}
