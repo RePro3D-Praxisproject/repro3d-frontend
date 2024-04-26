1558e8c13e2aa44eeb04f49de7f6c4c6a45200f5
mahlibalazs <mahli.balazs@outlook.hu>
2024-04-26 10:59:03 +0200

    fix: order service gets items from the api

--- src/app/shared/interfaces/item.ts
+++ src/app/shared/interfaces/item.ts
@@ -1,10 +1,16 @@
 export interface Item {
-    itemId: number,
+    item_id: number,
     title: string,
     price: number,
-    buildTime: number,
+    est_time: number,
     material: string,
     dimensions: string,
     description: string,
     imgUrl: string
+}
+
+export interface ItemResponse {
+    success: boolean,
+    message: string,
+    data: Item[]
 }
