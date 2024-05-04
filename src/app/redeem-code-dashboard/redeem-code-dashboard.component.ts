a4979ba0b4b9284b389f446d163b30ddc4f44c6d
Hosseinamir1989 <hosseinamir198968@gmail.com>
2024-05-04 11:24:29 +0200

    feat: add redeem code dashboard and correct function RP-52

--- src/app/redeem-code-dashboard/redeem-code-dashboard.component.ts
+++ src/app/redeem-code-dashboard/redeem-code-dashboard.component.ts
@@ -1,13 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import {RedeemCodes} from "../shared/interfaces/redeem-codes";
 import {redeemCodeService} from "../shared/services/redeem-code.service";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf, SlicePipe} from "@angular/common";
 
 @Component({
   selector: 'app-redeem-code-dashboard',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    NgIf,
+    SlicePipe
   ],
   templateUrl: './redeem-code-dashboard.component.html',
   styleUrl: './redeem-code-dashboard.component.scss'
@@ -16,7 +18,10 @@ import {NgForOf} from "@angular/common";
 
 export class RedeemCodeDashboardComponent implements OnInit {
   redeemCodes: RedeemCodes[] = [];
-
+  isLoading: boolean = false;
+  currentPage: number = 1;
+  itemsPerPage: number = 10;
+  totalItems: number = 0;
   constructor(private redeemCodeService: redeemCodeService) {}
 
   ngOnInit(): void {
@@ -24,31 +29,52 @@ export class RedeemCodeDashboardComponent implements OnInit {
   }
 
   loadRedeemCodes(): void {
-    this.redeemCodeService.loadAllRedeemCodes();
-    this.redeemCodes = this.redeemCodeService.redeemCodes;
-  }
+    this.isLoading = true;
+    this.redeemCodeService.loadAllRedeemCodes().subscribe(codes => {
+      this.redeemCodes = codes;
+        this.totalItems = codes.length;
+      this.isLoading = false;
+    }
 
-  editCode(code: RedeemCodes): void {
-    console.log('Edit functionality to be implemented', code);
+
+    );
+  }
+  onPageChange(page: number): void {
+    this.currentPage = page;
   }
 
   validateCode(code: string | undefined): void {
     if (code != null) {
+      this.isLoading = true;
+
       this.redeemCodeService.validateRedeemCode(code).subscribe(
         isValid => {
           alert(`Redeem code ${isValid ? 'is valid' : 'is invalid or already used'}`);
         }
       );
+      this.isLoading = false;
+
     }
   }
   generateCode(): void {
+    this.isLoading = true;
+
     this.redeemCodeService.generateRedeemCode();
+    this.isLoading = false;
+    this.loadRedeemCodes();
   }
 
 
   deleteCode(id: number | undefined): void {
     if (id) {
+      this.isLoading = true;
       this.redeemCodeService.deleteRedeemCode(id);
+      this.isLoading = false;
+      this.loadRedeemCodes();
+
     }
   }
+
+
+  protected readonly Math = Math;
 }
