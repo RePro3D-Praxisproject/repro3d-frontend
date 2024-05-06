5ca5a465ca8a384f55d53e7886e2da877e9541ac
Hosseinamir1989 <hosseinamir198968@gmail.com>
2024-05-06 23:12:05 +0200

    test: add basic unit tests to redeem code component RP-52

--- src/app/redeem-code-dashboard/redeem-code-dashboard.component.spec.ts
+++ src/app/redeem-code-dashboard/redeem-code-dashboard.component.spec.ts
@@ -1,23 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RedeemCodeDashboardComponent } from './redeem-code-dashboard.component';
+import { redeemCodeService } from '../shared/services/redeem-code.service';
+import {of, throwError} from 'rxjs';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
-import { RedeemcodeDashboardComponent } from './redeem-code-dashboard.component';
-
-describe('RedeemcodeDashboardComponent', () => {
-  let component: RedeemcodeDashboardComponent;
-  let fixture: ComponentFixture<RedeemcodeDashboardComponent>;
+describe('RedeemCodeDashboardComponent', () => {
+  let component: RedeemCodeDashboardComponent;
+  let fixture: ComponentFixture<RedeemCodeDashboardComponent>;
+  let mockRedeemCodeService: jasmine.SpyObj<redeemCodeService>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RedeemcodeDashboardComponent]
-    })
-    .compileComponents();
+    mockRedeemCodeService = jasmine.createSpyObj('redeemCodeService', {
+      'loadAllRedeemCodes': of([{ rc_code: "123xyz", rc_id: 12, used: 0 }]),
+      'validateRedeemCode': of(true),
+      'generateRedeemCode': of(null),
+      'deleteRedeemCode': of(null)
+    });
 
-    fixture = TestBed.createComponent(RedeemcodeDashboardComponent);
+    await TestBed.configureTestingModule({
+      imports: [
+        RedeemCodeDashboardComponent,
+        HttpClientTestingModule
+      ],
+      providers: [{ provide: redeemCodeService, useValue: mockRedeemCodeService }]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(RedeemCodeDashboardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it(' it should generate', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should validate redeem code', () => {
+    const spy = spyOn(window, 'alert');
+    component.validateCode('123xyz');
+    expect(mockRedeemCodeService.validateRedeemCode).toHaveBeenCalledWith('123xyz');
+    expect(spy).toHaveBeenCalledWith('Redeem code is valid');
+  });
+
+
+
+  it('should delete a redeem code', () => {
+    component.deleteCode(12);
+    expect(mockRedeemCodeService.deleteRedeemCode).toHaveBeenCalledWith(12);
+  })
+
+
 });
