7cbf76174ea01c34b4a9fdd54c375f971e687fc8
Hosseinamir1989 <hosseinamir198968@gmail.com>
2024-05-09 00:45:00 +0200

    fix: correct the service methods and dashboard component RP-52

--- src/app/redeem-code-dashboard/redeem-code-dashboard.component.ts
+++ src/app/redeem-code-dashboard/redeem-code-dashboard.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {RedeemCodes} from "../shared/interfaces/redeem-codes";
 import {redeemCodeService} from "../shared/services/redeem-code.service";
 import {NgForOf, NgIf, SlicePipe} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-redeem-code-dashboard',
@@ -9,7 +10,8 @@ import {NgForOf, NgIf, SlicePipe} from "@angular/common";
   imports: [
     NgForOf,
     NgIf,
-    SlicePipe
+    SlicePipe,
+    FormsModule
   ],
   templateUrl: './redeem-code-dashboard.component.html',
   styleUrl: './redeem-code-dashboard.component.scss'
@@ -22,64 +24,83 @@ export class RedeemCodeDashboardComponent implements OnInit {
   currentPage: number = 1;
   itemsPerPage: number = 10;
   totalItems: number = 0;
-  constructor(private redeemCodeService: redeemCodeService) {}
+  isModalOpen: boolean = false;
+  totalPages: number = 0;
+  validationMessage: string = '';
+  isValid: boolean = false;
+  constructor(protected redeemCodeService: redeemCodeService) {}
 
   ngOnInit(): void {
-    this.loadRedeemCodes();
+    this.redeemCodeService.loadAllRedeemCodes();
+    this.updateRedeemCodes();
   }
 
-  refreshPage(): void {
-    this.loadRedeemCodes();
-  }
-
-  loadRedeemCodes(): void {
+  updateRedeemCodes(): void {
     this.isLoading = true;
-    this.redeemCodeService.loadAllRedeemCodes().subscribe(codes => {
-      this.redeemCodes = codes;
-        this.totalItems = codes.length;
+    setTimeout(() => {
+      this.redeemCodes = this.redeemCodeService.getAllRedeemCodes();
+      this.totalItems = this.redeemCodes.length;
+      this.totalPages = Math.ceil(this.totalItems / this.itemsPerPage);
       this.isLoading = false;
-    }
-
-
-    );
+    }, 1000);
   }
+
+
+  toggleModal(open: boolean): void {
+    this.isModalOpen = open;
+  }
+
   onPageChange(page: number): void {
+    if (page < 1 || page > this.totalPages) {
+      return;
+    }
     this.currentPage = page;
-    this.refreshPage();
   }
 
   validateCode(code: string | undefined): void {
-    if (code != null) {
-      this.isLoading = true;
-
-      this.redeemCodeService.validateRedeemCode(code).subscribe(
-        isValid => {
-          alert(`Redeem code ${isValid ? 'is valid' : 'is invalid or already used'}`);
-        }
-      );
-      this.isLoading = false;
-
+    if (!code) {
+      this.validationMessage = 'No code provided.';
+      this.toggleModal(true);
+      return;
     }
+    this.isLoading = true;
+
+    this.redeemCodeService.validateRedeemCode(code).subscribe({
+      next: (response) => {
+        this.isValid = response.success === "true";
+        this.validationMessage = this.isValid ? 'Redeem code is active.' : response.message;
+        this.isLoading = false;
+        this.toggleModal(true);
+      },
+      error: (error) => {
+        this.validationMessage = 'Error validating code: ' + error.message;
+        this.isLoading = false;
+        this.toggleModal(true);
+      }
+    });
   }
   generateCode(): void {
     this.isLoading = true;
 
     this.redeemCodeService.generateRedeemCode();
     this.isLoading = false;
-    this.loadRedeemCodes();
+    this.redeemCodeService.getAllRedeemCodes();
   }
 
+  createCode(redeemCode: RedeemCodes){
+    this.redeemCodeService.createRedeemCode(redeemCode)
+    this.toggleModal(false);
+}
 
   deleteCode(id: number | undefined): void {
     if (id) {
       this.isLoading = true;
       this.redeemCodeService.deleteRedeemCode(id);
       this.isLoading = false;
-      this.loadRedeemCodes();
-      this.refreshPage();
+      this.redeemCodeService.getAllRedeemCodes();
     }
   }
 
-
   protected readonly Math = Math;
+
 }
--- src/app/shared/services/redeem-code.service.ts
+++ src/app/shared/services/redeem-code.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {catchError, map, Observable, throwError} from 'rxjs';
+import {catchError, Observable, throwError} from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { API_URL } from '../constants/apiurl.constant';
 import {RedeemCodes, RedeemCodeResponse} from "../interfaces/redeem-codes";
@@ -11,7 +11,7 @@ import {RedeemCodes, RedeemCodeResponse} from "../interfaces/redeem-codes";
 })
 export class redeemCodeService {
   constructor(private http: HttpClient) {}
-
+  public redeemCodes: RedeemCodes[] = [];
 
 
 
@@ -24,25 +24,26 @@ export class redeemCodeService {
 
   }
 
-  loadAllRedeemCodes(): Observable<RedeemCodes[]> {
+
+  public getAllRedeemCodes(): RedeemCodes[]{
+    return this.redeemCodes;
+  }
+
+  public loadAllRedeemCodes(): void {
     console.log('Calling API');
-    return this.http.get<RedeemCodeResponse>(`${API_URL}/redeem-code`).pipe(
-      map(response => {
-        console.log('API response:', response);
-        return response.data;
-      }),
-      catchError(error => {
-        console.error('API error:', error);
-        return throwError(() => new Error('Error fetching redeem codes'));
-      })
-    );
+    this.http.get<RedeemCodeResponse>(`${API_URL}/redeem-code`).subscribe(
+      redeemCodes => {
+        this.redeemCodes= redeemCodes.data;
+        console.log(this.redeemCodes)
+      }
+    )
   }
 
   public generateRedeemCode(): void {
     this.http.get<RedeemCodeResponse>(`${API_URL}/redeem-code/generate`).subscribe(
       response => {
         console.log('Generated Redeem Code:', response);
-        this.loadAllRedeemCodes();  // Optionally refresh list
+        this.loadAllRedeemCodes();
       },catchError(error => {
       console.error('generate error:', error);
       return throwError(error);
@@ -50,18 +51,11 @@ export class redeemCodeService {
     );
   }
 
-  public validateRedeemCode(code: string): Observable<boolean> {
-    const headers = new HttpHeaders().set('RC-Code', code);
-    return this.http.get<RedeemCodeResponse>(`${API_URL}/redeem-code/validate`, { headers }).pipe(
-      map(response => {
-        return response.success === 'true';
-      }),
-      catchError(error => {
-        console.error('Validation error:', error);
-        return throwError(error);
-      })
-    );
+  public validateRedeemCode(code: string ): Observable<RedeemCodeResponse> {
+    let headers = new HttpHeaders().set('RC-Code', code);
+    return this.http.get<RedeemCodeResponse>(`${API_URL}/redeem-code/validate`, { headers });
   }
+
 
 
   public updateRedeemCode(redeemCode: RedeemCodes, id: number | undefined): void {
