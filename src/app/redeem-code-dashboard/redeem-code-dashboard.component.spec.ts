0a8b83b9fe9482eb43c22c5e6f89b13f4051d4eb
Hosseinamir1989 <hosseinamir198968@gmail.com>
2024-05-09 20:53:41 +0200

    fix(test): correct the tests after change methods RP-52

--- src/app/redeem-code-dashboard/redeem-code-dashboard.component.spec.ts
+++ src/app/redeem-code-dashboard/redeem-code-dashboard.component.spec.ts
@@ -1,52 +1,71 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RedeemCodeDashboardComponent } from './redeem-code-dashboard.component';
 import { redeemCodeService } from '../shared/services/redeem-code.service';
-import {of, throwError} from 'rxjs';
 import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { of } from 'rxjs';
+import { RedeemCodes } from "../shared/interfaces/redeem-codes";
 
 describe('RedeemCodeDashboardComponent', () => {
   let component: RedeemCodeDashboardComponent;
   let fixture: ComponentFixture<RedeemCodeDashboardComponent>;
   let mockRedeemCodeService: jasmine.SpyObj<redeemCodeService>;
+  let mockRedeemCodes: RedeemCodes[] = [
+    { rc_id: 1, rcCode: "CODE1", used: 0 },
+    { rc_id: 2, rcCode: "CODE2", used: 1 }
+  ];
 
   beforeEach(async () => {
-    mockRedeemCodeService = jasmine.createSpyObj('redeemCodeService', {
-      'loadAllRedeemCodes': of([{ rc_code: "123xyz", rc_id: 12, used: 0 }]),
-      'validateRedeemCode': of(true),
-      'generateRedeemCode': of(null),
-      'deleteRedeemCode': of(null)
+    mockRedeemCodeService = jasmine.createSpyObj('redeemCodeService', [
+      'getAllRedeemCodes',
+      'loadAllRedeemCodes',
+      'validateRedeemCode',
+      'generateCode',
+      'createRedeemCode',
+      'deleteRedeemCode'
+    ]);
+    mockRedeemCodeService.validateRedeemCode.and.returnValue(of({
+      success: "true",
+      message: "Redeem code is active.",
+      data: []
+    }))
+    mockRedeemCodeService.loadAllRedeemCodes.and.callFake(() => {
+      mockRedeemCodeService.getAllRedeemCodes.and.returnValue(mockRedeemCodes);
+      component.redeemCodes = mockRedeemCodes;
     });
-
     await TestBed.configureTestingModule({
       imports: [
-        RedeemCodeDashboardComponent,
-        HttpClientTestingModule
+        HttpClientTestingModule,
+        RedeemCodeDashboardComponent // Import the standalone component here
       ],
-      providers: [{ provide: redeemCodeService, useValue: mockRedeemCodeService }]
+      providers: [
+        { provide: redeemCodeService, useValue: mockRedeemCodeService }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(RedeemCodeDashboardComponent);
     component = fixture.componentInstance;
+
+
+
     fixture.detectChanges();
   });
 
-  it(' it should generate', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should validate redeem code', () => {
-    const spy = spyOn(window, 'alert');
-    component.validateCode('123xyz');
-    expect(mockRedeemCodeService.validateRedeemCode).toHaveBeenCalledWith('123xyz');
-    expect(spy).toHaveBeenCalledWith('Redeem code is valid');
+  it('should load redeem codes on init', () => {
+    component.ngOnInit();
+    expect(mockRedeemCodeService.loadAllRedeemCodes).toHaveBeenCalled();
+    expect(component.redeemCodes).toEqual(mockRedeemCodes);
   });
 
 
-
-  it('should delete a redeem code', () => {
-    component.deleteCode(12);
-    expect(mockRedeemCodeService.deleteRedeemCode).toHaveBeenCalledWith(12);
-  })
-
+  it('should handle redeem code validation', () => {
+    component.validateCode('CODE1');
+    expect(mockRedeemCodeService.validateRedeemCode).toHaveBeenCalledWith('CODE1');
+    expect(component.validationMessage).toEqual('Redeem code is active.');
+    expect(component.isValid).toBeTrue();
+  });
 
 });
