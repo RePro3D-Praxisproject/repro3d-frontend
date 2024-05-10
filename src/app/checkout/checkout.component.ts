e1b21c3930f9f5d9b468c20b8766d846118d29d1
Máhli Balázs <160179105+mahlibalazs@users.noreply.github.com>
2024-05-10 13:12:55 +0200

    feat: add checkout component

---
+++ src/app/checkout/checkout.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Item } from '../shared/interfaces/item';
+import { ActivatedRoute } from '@angular/router';
+import { OrderService } from '../shared/services/order.service';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-checkout',
+  standalone: true,
+  imports: [
+    NgIf
+  ],
+  templateUrl: './checkout.component.html',
+  styleUrl: './checkout.component.scss'
+})
+export class CheckoutComponent implements OnInit {
+  product!: Item;
+  userData = JSON.parse(localStorage.getItem('userdata')!);
+
+  constructor(
+    private route: ActivatedRoute,
+    public orderService: OrderService
+  ) {}
+
+  ngOnInit(): void {
+    
+    const productId = parseInt(this.route.snapshot.paramMap.get('id')!);
+    this.orderService.getItemByIdAsync(productId).subscribe(
+      p => {
+        this.product = p.data;
+      }
+    )
+  }
+}
