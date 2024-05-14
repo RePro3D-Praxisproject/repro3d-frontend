3accbc57afad4f0daf55753c477e3203346eb04e
Máhli Balázs <160179105+mahlibalazs@users.noreply.github.com>
2024-05-14 11:41:48 +0200

    feat: add order history component

--- src/app/order-history/order-history.component.ts
+++ src/app/order-history/order-history.component.ts
@@ -1,62 +1,31 @@
-import {Component, OnInit} from '@angular/core';
-import {OrderService} from "../shared/services/order.service";
-import {Receipt} from "../shared/interfaces/receipt";
-import {ReceiptService} from "../shared/services/receipt.service";
-
-class Order {
-}
+import { Component, OnInit } from '@angular/core';
+import { OrderService } from "../shared/services/order.service";
+import { Order } from '../shared/interfaces/order';
+import { NgFor } from '@angular/common';
+import { OrderItemsComponent } from "./order-items/order-items.component";
 
 @Component({
-  selector: 'app-order-history',
-  standalone: true,
-  imports: [],
-  templateUrl: './order-history.component.html',
-  styleUrl: './order-history.component.scss'
+    selector: 'app-order-history',
+    standalone: true,
+    templateUrl: './order-history.component.html',
+    styleUrl: './order-history.component.scss',
+    imports: [NgFor, OrderItemsComponent]
 })
 export class OrderHistoryComponent implements OnInit{
 
-  receipts: Receipt[] = [];
-
   orders: Order[] = [];
-  private isModalOpen: boolean = false;
+
+  constructor(public orderService: OrderService) {}
+
   ngOnInit(): void {
-    this.loadReceipts();
-  }
-
-  constructor(private receiptService: ReceiptService) {}
-
-  private loadReceipts() {
-    this.receiptService.getAllReceipts().subscribe({
-      next: (response) => {
-        if (response.success) {
-          this.receipts = response.data;
-        } else {
-          console.error('Failed to load receipts:', response.message);
-        }
-      },
-      error: (err) => console.error('Error fetching receipts:', err)
-    });
-  }
-
-  updateReceipt(id: number, receiptDetails: Receipt): void {
-    this.receiptService.updateReceipt(id, receiptDetails);
-    this.toggleModal(false)
-  }
-  deleteReceipt(id: number): void {
-    this.receiptService.deleteReceipt(id).subscribe({
-      next: (response) => {
-        if (response.success) {
-          console.log('Receipt deleted successfully:', response.message);
-        } else {
-          console.error('Failed to delete receipt:', response.message);
-        }
-      },
-      error: (err) => console.error('Error deleting receipt:', err)
-    });
+    this.orderService.getOrdersByEmail("mahli@posteo.net").subscribe(
+      res => {
+        this.orders = res.data;
+        console.log(this.orders)
+      }
+    )
   }
 
 
-  private toggleModal(open: boolean) {
-    this.isModalOpen = open;
-  }
+  
 }
