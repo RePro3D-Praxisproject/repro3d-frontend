c265614ba51df6f345b7dacbdfc784c0850d4ee1
Máhli Balázs <160179105+mahlibalazs@users.noreply.github.com>
2024-05-14 12:55:05 +0200

    feat: add no available orders message when history is empty

--- src/app/order-history/order-history.component.ts
+++ src/app/order-history/order-history.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderService } from "../shared/services/order.service";
 import { Order } from '../shared/interfaces/order';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { OrderItemsComponent } from "./order-items/order-items.component";
 
 @Component({
@@ -9,19 +9,21 @@ import { OrderItemsComponent } from "./order-items/order-items.component";
     standalone: true,
     templateUrl: './order-history.component.html',
     styleUrl: './order-history.component.scss',
-    imports: [NgFor, OrderItemsComponent]
+    imports: [NgFor, OrderItemsComponent, NgIf]
 })
 export class OrderHistoryComponent implements OnInit{
 
   orders: Order[] = [];
 
-  constructor(public orderService: OrderService) {}
+  constructor(public orderService: OrderService
+  ) {
+    this.orderService.loadAllOrders();
+  }
 
   ngOnInit(): void {
-    this.orderService.getOrdersByEmail("mahli@posteo.net").subscribe(
+    this.orderService.getOrdersByEmail(localStorage.getItem("email")!).subscribe(
       res => {
         this.orders = res.data;
-        console.log(this.orders)
       }
     )
   }
